2369a335991c6709c8e7deeb4fe1ef96d941955d
KishiKyousuke（Kyo18） <59789739+KishiKyousuke@users.noreply.github.com>
2023-01-20 08:56:26 +0900

    lec.54

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,22 @@ import axios from "axios";
 import { useState } from "react";
 import { Todo } from "./Todo";
 
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
 export default function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
 
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      setTodos(res.data);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
   };
 
   return (
