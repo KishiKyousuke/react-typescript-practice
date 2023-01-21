d004cd5c553797c32028b12343ee524df18114e3
KishiKyousuke（Kyo18） <59789739+KishiKyousuke@users.noreply.github.com>
2023-01-21 13:07:57 +0900

    ~lec.59

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import { useState } from "react";
 import { Todo } from "./Todo";
 import { TodoType } from "./types/todo";
 import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./types/user";
+
+const user: User = {
+  name: "Kyo"
+  //hobbies: ["野球観戦", "音楽"]
+};
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -18,6 +25,7 @@ export default function App() {
 
   return (
     <div className="App">
+      <UserProfile user={user} />
       <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
